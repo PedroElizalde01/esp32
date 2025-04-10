20d2552e107d3ba6d4197efda7e162fa6fc4aa80
PedroElizalde01 <pesro.elizalde@ing.austral.edu.ar>
2025-04-10 10:25:53 -0300

    telegram: sendLocation, sendAudio

---
+++ telegram/sendAudio.ino
@@ -0,0 +1,94 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "nombre de tu wifi";     // CHANGE
+const char* password = "contraseña"; // CHANGE
+const char* botToken = "telegram token";   // CHANGE
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(botToken, client);
+
+const unsigned long checkInterval = 1000;
+unsigned long lastCheckTime = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println();
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println();
+  Serial.println("WiFi conectado");
+  Serial.print("IP local: ");
+  Serial.println(WiFi.localIP());
+
+  client.setInsecure();
+  
+  Serial.println("Bot de Telegram iniciado.");
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+  
+  if (currentTime - lastCheckTime > checkInterval) {
+    lastCheckTime = currentTime;
+
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    for (int i = 0; i < numNewMessages; i++) {
+      String chat_id = bot.messages[i].chat_id;
+      String text = bot.messages[i].text;
+      String from_name = bot.messages[i].from_name;
+
+      Serial.println("Nuevo mensaje:");
+      Serial.println("De: " + from_name);
+      Serial.println("Mensaje: " + text);
+
+      enviarUbicacion(chat_id);
+    }
+  }
+}
+
+void enviarAudioURL(String chat_id) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient https;
+    String url = "https://api.telegram.org/bot" + String(botToken) + "/sendAudio";
+
+    https.begin(url);
+    https.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // public mp3 url
+    String audio_url = "https://www.sample-videos.com/audio/mp3/crowd-cheering.mp3"; //example
+
+    String payload = "chat_id=" + chat_id +
+                     "&audio=" + audio_url;
+
+    int httpResponseCode = https.POST(payload);
+
+    if (httpResponseCode > 0) {
+      String response = https.getString();
+      Serial.println("Respuesta Telegram:");
+      Serial.println(response);
+    } else {
+      Serial.print("Error en POST: ");
+      Serial.println(httpResponseCode);
+    }
+    https.end();
+  } else {
+    Serial.println("WiFi desconectado");
+  }
+}
+
